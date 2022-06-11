cab3d2a189e50529bf654d1a75f63466d8c67e8e
Luis Chang <luis.chang@outlook.com>
2022-06-11 11:01:40 -0500

    Create customer in modal

--- Semana11UESANMVC.WEB/Controllers/CustomerController.cs
+++ Semana11UESANMVC.WEB/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Semana11UESANMVC.WEB.Models;
 using Semana11UESANMVC.WEB.Services;
 
 namespace Semana11UESANMVC.WEB.Controllers
@@ -17,6 +18,27 @@ namespace Semana11UESANMVC.WEB.Controllers
             return PartialView(customers);
         }
 
-        
+        [HttpPost]
+        public async Task<IActionResult> Create(int idCliente, string nombre,
+                string apellido, string ciudad, string telefono, string pais) 
+        {
+            var customer = new CustomerModel()
+            {
+                FirstName = nombre,
+                LastName = apellido,
+                City = ciudad,
+                Country = pais,
+                Phone = telefono
+            };
+
+            bool exito = true;
+            if (idCliente == -1)
+                exito = await CustomerService.Create(customer) > 0 ? true: false;
+            else {
+                customer.Id = idCliente;
+                exito = await CustomerService.Update(customer) > 0 ? true : false;
+            }
+            return Json(exito);        
+        }        
     }
 }
--- Semana11UESANMVC.WEB/Services/CustomerService.cs
+++ Semana11UESANMVC.WEB/Services/CustomerService.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using Semana11UESANMVC.WEB.Models;
+using System.Text;
 
 namespace Semana11UESANMVC.WEB.Services
 {
@@ -15,6 +16,46 @@ namespace Semana11UESANMVC.WEB.Services
             string apiResponse = response.Content.ReadAsStringAsync().Result;
             var customers = JsonConvert.DeserializeObject<IEnumerable<CustomerModel>>(apiResponse);
             return customers;
+        }
+
+        //Create CustomerModel
+        public static async Task<int> Create(CustomerModel customer)
+        {
+            string urlBase = "http://localhost:5257/api/Customer/";
+
+            var json = JsonConvert.SerializeObject(customer);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            using var httpClient = new HttpClient();
+            using var response = await httpClient.PostAsync(urlBase + "Create", data);
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            var customerResponse = JsonConvert.DeserializeObject<int>(apiResponse);
+
+            return customerResponse;
+        }
+
+        //Update CustomerModel
+        public static async Task<int> Update(CustomerModel customer) 
+        {
+            string urlBase = "http://localhost:5257/api/Customer/";
+            var json = JsonConvert.SerializeObject(customer);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            using var httpClient = new HttpClient();
+            using var response = await httpClient.PutAsync(urlBase + "Update", data);
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            var customerResponse = JsonConvert.DeserializeObject<int>(apiResponse);
+            return customerResponse;
+        }
+
+        //Delete CustomerModel
+        public static async Task<bool> Delete(int id)
+        {
+            string urlBase = "http://localhost:5257/api/Customer/";
+            using var httpClient = new HttpClient();
+            using var response = await httpClient.DeleteAsync(urlBase + "Delete/" + id);
+            string apiResponse = await response.Content.ReadAsStringAsync();
+
+            return ((int)response.StatusCode == 400 ? false : true);
 
         }
 
