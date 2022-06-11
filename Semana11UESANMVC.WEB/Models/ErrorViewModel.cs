51335d236ed678b0f733270193ebd2ef4eba6506
Luis Chang <luis.chang@outlook.com>
2022-06-11 07:53:36 -0500

    Agregar archivos de proyecto.

---
+++ Semana11UESANMVC.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Semana11UESANMVC.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Semana11UESANMVC.WEB/Services/CustomerService.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using Semana11UESANMVC.WEB.Models;
+
+namespace Semana11UESANMVC.WEB.Services
+{
+    public class CustomerService
+    {     
+        public static async Task<IEnumerable<CustomerModel>> GetAll()
+        {
+            //Get all customers
+            string urlBase = "http://localhost:5257/api/Customer/";
+            
+            var client = new HttpClient();
+            var response = await client.GetAsync(urlBase + "GetAll");
+            string result = await response.Content.ReadAsStringAsync();
+            var customers = JsonConvert.DeserializeObject<IEnumerable<CustomerModel>>(result);
+            return customers;
+        }
+
+
+
+    }
+}
