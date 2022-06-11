1b9ead09db702e5a3fbc7b106ead2a1fbf37ea74
Luis Chang <luis.chang@outlook.com>
2022-06-11 08:30:20 -0500

    Listado cliente (action y table)

---
+++ Semana11UESANMVC.WEB/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Semana11UESANMVC.WEB.Services;
+
+namespace Semana11UESANMVC.WEB.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Listado()
+        {
+            var customers = await CustomerService.GetAll();
+            return View(customers);
+        }
+
+        
+    }
+}
